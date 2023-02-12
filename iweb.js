ae2e3ea9057d0bfb95983904671b4bbd085ca28c
naqiraza512 <117478002+naqiraza512@users.noreply.github.com>
2023-02-13 00:34:40 +0500

    Add files via upload

--- iweb.js
+++ iweb.js
@@ -7,6 +7,12 @@ var header = document.querySelector("header");
 header.addEventListener("click", function() {
     if(header.style.color=="black")
   header.style.color = "red";
+  else if(header.style.color=="red")
+  header.style.color = "green";
+  else if(header.style.color=="green")
+  header.style.color = "yellow";
+  else if(header.style.color=="yellow")
+  header.style.color = "darkblue";
   else
   header.style.color = "black";
 });
